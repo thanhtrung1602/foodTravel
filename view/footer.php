807d7aca7d921090e35e4f44b1870bd4904a5291
Giahuy <nguyenvangiahuy1988@gmail.com>
2023-11-14 19:22:11 +0700

    update

---
+++ view/footer.php
@@ -0,0 +1,116 @@
+<footer>
+            <div class="footer-header">
+                <img src="./img/logo/logo_main.png" alt="">
+            </div>
+            <section class="footer">
+              <div class="footer-left">
+                <h2>Khám phá</h2>
+                <ul class="footer-ul">
+                    <li class="footer-item">
+                        <a href="">Tạo bộ sưu tập</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="">Bảo mật thông tin</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="">Quy định</a>
+                    </li>
+                </ul>
+              </div>
+
+              <div class="footer-middle">
+                <h2>Công ty</h2>
+                <ul class="footer-ul">
+                    <li class="footer-item">
+                        <a href="">Giới thiệu</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="">Trợ giúp</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="">Việc làm</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="">Quy chế</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="">Thỏa thuận dịch vụ</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="">Liên hệ</a>
+                    </li>
+                </ul>
+              </div>
+
+              <div class="footer-right">
+                <h2>Cộng đồng</h2>
+                <ul class="footer-ul">
+                    <li class="footer-item">
+                        <a href="">Facebook</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="">Instagram</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="">Youtube</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="">Tiktok</a>
+                    </li>
+                </ul>
+              </div>
+
+              
+              <div class="footer-last">
+                <h2>Giấy phép</h2>
+                <ul class="footer-ul">
+                    <li class="footer-item">
+                        <a href="">TRUNGDEPTRAICAP</a>
+                    </li>
+                    <!-- <li class="footer-item">
+                        <img src="./img/logo/pinterest_board_photo.png" alt="">
+                    </li> -->
+                </ul>
+              </div>
+                   <!-- <div>
+                    <hr />
+                    <p>Ứng dụng mobile</p>
+                    <p>Bộ sưu tập</p>
+                    <p>Bảo mật thông tin</p>
+                    <p>Quy định</p>
+                   </div> &nbsp;&nbsp;&nbsp;&nbsp;
+                   <div>
+                    <h2>Công ty</h2>
+                    <hr />
+                    <p>Giới thiệu</p>
+                    <p>Trợ giúp</p>
+                    <p>việc làm</p>
+                    <p>Quy chế</p>
+                    <p>Thỏa thuận sử dụng dịch vụ</p>
+                   <p>Liên hệ</p>
+                   </div> &nbsp;&nbsp;&nbsp;&nbsp;
+                   <div>
+                     <h2>Tham gia trên</h2>
+                    <hr />
+                    <p>Facebook</p>
+                    <p>Instagram</p>
+                    <p>Youtube</p>
+                    <p>Google</p>
+                    <p>ShopeeFood.vn-Giao đồ ăn tận nơi</p>
+                   <p>Liên hệ</p>
+                   </div> &nbsp;&nbsp;&nbsp;&nbsp;
+                   <div> 
+                    <h2>Giấy phép</h2>
+                    <hr />
+                    <p>MXH 363/GP-BTTTT</p>
+                   <p><img src="https://www.foody.vn/style/images/gov_seals.jpg" width="150px" height="90px"></p>
+                   
+                   </div> -->
+               
+            </section>
+        </footer>
+    </div>
+    <script src="./layout/assets/main.js"></script>
+</body>
+
+</html>
