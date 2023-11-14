d317c9d67e672ffb9c23045b36eb1671b2fe5020
Nguyen Thanh Trung <thanhtrung160204@gmail.com>
2023-11-14 11:07:39 +0700

    done

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,27 @@
 // xu li like anh 
-
 const likes = document.querySelectorAll('.fa-thumbs-up');
 const likeNumbers = document.querySelectorAll('.like-icon');
-let  numLikes = document.querySelectorAll('.numLike')
+// let  numLikes = document.querySelectorAll('.numLike')
 likes.forEach(function(like, index) {
-    let numLike = likeNumbers[index]
-    let likeNum = likeNumbers[index]
-
-    likeNum = 0
-    like.onclick = () => {
-        if (like.style.color === "rgb(128, 128, 128)" || like.style.color === "#808080") {
-            like.style.color = "#1877F2"
-            likeNum ++;
-        } 
-        numLike.innerHTML = likeNum
+    let numLike = parseInt(likeNumbers[index].textContent);
+    let storedLike = localStorage.getItem(`like${index}`);
+    if (storedLike) {
+        numLike = parseInt(storedLike);
+        like.style.color = "#1877F2";
     }
+    like.onclick = () => {
+        if (like.style.color === 'rgb(128, 128, 128)' || like.style.color === "#808080") {
+            numLike = 0
+            numLike ++;
+            likeNumbers[index].textContent = numLike;
+            like.style.color = "#1877F2";
+            localStorage.setItem(`like${index}`, numLike);
+        } else {
+            numLike --;
+            likeNumbers[index].textContent = numLike;
+            like.style.color = "#808080";
+            localStorage.removeItem(`like${index}`)
+        }
+    }
+    localStorage.setItem('num',numLike)
 })
