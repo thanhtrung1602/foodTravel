32719cecc3bab1b85d178680040a0573f939665d
Nguyen Thanh Trung <thanhtrung160204@gmail.com>
2023-11-14 18:45:47 +0700

    update

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    include_once './model/connect.php';
+    include_once './model/monan.php';
+
+    if(isset($_GET['pg'])&&($_GET['pg'])) {
+        $pg = $_GET['pg'];
+        switch ($pg) {
+            case 'value':
+                
+                break;
+            
+            default:
+            
+                break;
+        }
+    }
+    
+?>
