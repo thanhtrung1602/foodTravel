59958d736241ed38e9a28f944b4c66d6d3ef8b91
tuan_account <ptuanpk@gmail.com>
2023-11-23 12:31:38 +0700

    xong danhmuc

---
+++ admin/view/updatedmform.php
@@ -0,0 +1,59 @@
+
+
+
+
+
+<div class="main--content">
+        <div class="header--wrapper">
+            <div class="header--title">
+                <h1>CẬP NHẬT DANH MỤC</h1>
+            </div>
+            <div class="user--info">
+                <div class="box-search">
+                    <ion-icon name="search-outline"></ion-icon>
+                    <input type="text" placeholder="Tìm Kiếm">
+                </div>
+                <img src="view/layout/img/tom.jfif" alt="">
+            </div>
+        </div>
+        <div class="main--wrapper">
+            <div class="table--container">  
+            <form action="index.php?pg=updatedmform" method="post">
+        <input type="text" name="tendm" id="" value="<?=$kqone[0]['tendm']?>">
+        <input type="hidden" name="id" value="<?=$kqone[0]['id']?>">
+        <input type="submit" name="capnhat" value="Cập nhật">
+    </form>
+                <table>
+                    <thead>
+                    <tr>
+            <th>STT</th>
+            <th>Tên danh mục</th>
+            <th>Ưu tiên</th>
+            <th>Hiển thị</th>
+            <th>Hành động</th>
+        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            if(isset($kq)&&(count($kq)>0)){
+                                $i=1;
+                                foreach ($kq as $dm){
+                                    echo '<tr>
+                                        <td>'.$i.'</td>
+                                        <td>'.$dm['tendm'].'</td>
+                                        <td>'.$dm['uutien'].'</td>
+                                        <td>'.$dm['hienthi'].'</td>
+                                        <td><a href="index.php?pg=updatedmform&id='.$dm['id'].'">Sửa</a>|<a href="index.php?pg=deletedm&id='.$dm['id'].'">Xóa</a></td>
+                                    </tr>';
+                                $i++;
+                                }
+                            }
+                            ?>
+                          
+                </table>
+            </div>
+            </tbody> 
+        </div>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,6 +2,8 @@
     include_once './model/connect.php';
     include_once './model/monan.php';
     include_once "view/header.php";
+    include_once './model/catalog.php';
+  
     if(isset($_GET['pg'])&&($_GET['pg'])) {
         $pg = $_GET['pg'];
         switch ($pg) {
@@ -11,13 +13,28 @@
             
             default:
             
-                break;
+                break;  
         }
     }else{
+        $result = queryToGetResult();
+
+
         include_once "view/home.php";
     }
 
     include_once "view/footer.php";
+
+
+    function queryToGetResult() {
+        $conn=connectdb();
+        $stmt = $conn->prepare("SELECT * FROM catalog");
+        $stmt->execute();
+        $kq=$stmt->fetchAll();
+        return $kq;
+
+        
+        return $kq;
+    }
 
     
 ?>
