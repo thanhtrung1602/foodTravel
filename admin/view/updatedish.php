0e440d1cc16255fb322d1089b2a50869f9a7bb1d
Giahuy <nguyenvangiahuy1988@gmail.com>
2023-11-28 14:48:45 +0700

    update

--- admin/view/updatedish.php
+++ admin/view/updatedish.php
@@ -18,27 +18,25 @@
             <div class="table--container">
                 <table class="form--nhap">
                     <thead>
-                        <tr>
                             <form action="index.php?pg=updatedish" method="post">
-                                <th><h4>Mã: </h4>
-                                <input type="hidden" name="id_dish" value="<?=$kqgetone [0]['id']?>"></th>                              
-                                <th> <h4>Tên: </h4>
-                                <input type="text" name="name_dish" value="<?=$kqgetone [0]['name']?>"> </th>
-                                <th><h4>Giá: </h4>
-                                <input type="text" name="price_dish" value="<?=$kqgetone [0]['price']?>"></th>
-                                <th><h4>Hình: </h4>
-                                <input type="file" name="img_dish" value="<?=$kqgetone [0]['img']?>"> <br></th>
-                                <th><h4>Mã quán: </h4>
-                                <input type="text" name="id_eatery" value="<?=$kqgetone [0]['id_eatery']?>"></th>
-                                <th><h4>Mã loại: </h4>
-                                <input type="text" name="id_catalog" value="<?=$kqgetone [0]['id_catalog']?>"></th>
-                                <th><h4>Thêm: </h4>
-                                <input type="submit" name="capnhat" value="Cập nhật"></th></th>     
+                                <tr><h4>Mã: </h4>
+                                <input type="hidden" name="id_dish" value="<?=$kqgetone [0]['id']?>"></tr>                              
+                                <tr> <h4>Tên: </h4>
+                                <input type="text" name="name_dish" value="<?=$kqgetone [0]['name']?>"> </tr>
+                                <tr><h4>Giá: </h4>
+                                <input type="text" name="price_dish" value="<?=$kqgetone [0]['price']?>"></tr>
+                                <tr><h4>Hình: </h4>
+                                <input type="file" name="img_dish" value="<?=$kqgetone [0]['img']?>"> <br></tr>
+                                <tr><h4>Mã quán: </h4>
+                                <input type="text" name="id_eatery" value="<?=$kqgetone [0]['id_eatery']?>"></tr>
+                                <tr><h4>Mã loại: </h4>
+                                <input type="text" name="id_catalog" value="<?=$kqgetone [0]['id_catalog']?>"></tr>
+                                <tr><h4>Thêm: </h4>
+                                <input type="submit" name="capnhat" value="Cập nhật"></tr></tr>     
                             </form>
-                        </tr>
                     </thead>
                 </table> 
-                <table>
+                <!-- <table>
                     <thead>
                         <tr>
                             <th>Mã</th>
@@ -67,7 +65,7 @@
                                 }
                             }    
                         ?>
-                </table>
+                </table> -->
             </div>
             </tbody> 
         </div>
